8c2089a557bd837723eaa77484e004982d9708cc
Flulike <gyp12138@163.com>
2025-03-19 19:15:48 +0900

    test

--- train_detr.py
+++ train_detr.py
@@ -1,13 +1,14 @@
 from ultralytics import RTDETR
 
-device = 3
+device = [2]
 
 # Load a model
 model = RTDETR('rtdetr-l.yaml')  # build a new model from YAML
-# model = RTDETR("rtdetr-l.pt")  # load a pretrained model (recommended for training)
-# model = RTDETR("rtdetr-l.yaml").load("rtdetr-l.pt")  # build from YAML and transfer weights
+model = RTDETR("rtdetr-l.pt")  # load a pretrained model (recommended for training)
+model = RTDETR("rtdetr-l.yaml").load("rtdetr-l.pt")  # build from YAML and transfer weights
 
 project = "/home/guo/ultralytics/results/ultralytics/RTDETR"
+optimizer = "Adamw"
 
 # Train the model
-results = model.train(data="/home/guo/ultralytics/ultralytics/cfg/datasets/_visdrone.yaml", epochs=300, imgsz=640, device=device, project=project, batch=4, name = "rtdetr-l_visdrone_vml6")
+results = model.train(data="/home/guo/ultralytics/ultralytics/cfg/datasets/_visdrone.yaml", epochs=300, imgsz=640, device=device, project=project, batch=4, amp=False, optimizer=optimizer, name="rtdetr-l_visdrone_vml6_")
