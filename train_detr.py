0e8cf27e5d1cb6e742685a93905eec3687673bfb
Flulike <gyp12138@163.com>
2025-07-02 17:45:55 +0900

    Auto backup on 2025-07-02 17:45:55

--- train_detr.py
+++ train_detr.py
@@ -6,32 +6,32 @@ import torch
 
 
 
-warnings.filterwarnings('ignore')
+import os
+import sys
 
-
-def check_path(path):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Path does not exist: {path}")
-
+# 添加项目根目录到Python路径
+project_root = os.path.dirname(os.path.abspath(__file__))
+os.environ["PYTHONPATH"] = f"{project_root}:{os.environ.get('PYTHONPATH', '')}"
 
 if __name__ == '__main__':
     torch.cuda.empty_cache()
     # 获取当前脚本所在的目录
     current_dir = Path(__file__).parent
     # 构建相对路径
-    yaml_path = 'ultralytics/cfg/datasets/Fisheye.yaml'
-    check_path(yaml_path)
+    yaml_path = 'ultralytics/cfg/datasets/VisDrone.yaml'
     # model = RTDETR('ultralytics/cfg/models/uavdetr-r50.yaml')
-    model = RTDETR('rtdetr-resnet101.yaml')
-    # model = RTDETR("rtdetr-l.pt")  # load a pretrained model (recommended for training)
-    # model = RTDETR("rtdetr-l.yaml").load("rtdetr-l.pt")  # build from YAML and transfer weights
-    device = [3]
+    model = RTDETR('rtdetr-l.yaml')
+    model = RTDETR("rtdetr-l.pt")  # load a pretrained model (recommended for training)
+    model = RTDETR("rtdetr-l.yaml").load("rtdetr-l.pt")  # build from YAML and transfer weights
+    device = [2, 3]
     optimizer = 'Adamw'
     model.train(data=str(yaml_path),
-                imgsz=640,
                 epochs=100,
+                batch=16,
                 device=device,
+                optimizer=optimizer,
                 # resume='', # last.pt path
                 project='results/ultralytics/RTDETR',
-                name='101_fisheye_vml4_',
+                name='l_visdrone_vml4_',
+                amp=False,
                 )
